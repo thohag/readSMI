a4db3cd66f8d5facf524c3a9e4437120fc36466e
thohag <tmshag1@gmail.com>
2015-11-14 05:33:58 +0100

    Improved support for older file formats and files without MSG tags.

--- R/readSMI.R
+++ R/readSMI.R
@@ -2,45 +2,51 @@
 #' 
 #' This function reads SMI eye-tracker files
 #' @param file smi filename to read.
+#' @param msgcolposition column of MSG data (optional)
+#' @param msgprefix prefix to remove from MSG data (optional)
 #' @examples
 #' data = readSMI("AXCPT_Subject_1 Samples.txt")
 
 
 
-readSMI = function(file) {
+readSMI = function(file, msgcolposition=4, msgprefix="# Message: ") {
   
-  datas <- readFast(file)
+  datas <- readFast(file, msgcolposition)
   #datas <- data.table(read.csv(file=file, header=T, sep="\t", skip=38, stringsAsFactors=FALSE))
-  msgcol = names(datas)[4]
+  msgcol = names(datas)[msgcolposition]
+  
+  if (nrow(head(datas[Type == "MSG",])) > 0) {
     
-  start = nchar("# Message: ")+1
-  datas[Type == "MSG", MSG:=substring(get(msgcol),start) ,by=Time]
-  #datas[Type == "MSG", MSG:=strsplit(get(msgcol),"# Message: ")[[1]][2] ,by=Time]
-  
-  repeat.before = function(x) {   # repeats the last non NA value. Keeps leading NA
-    ind = which(!is.na(x))      # get positions of nonmissing values
-    if(is.na(x[1]))             # if it begins with a missing, add the 
-      ind = c(1,ind)        # first position to the indices
-    rep(x[ind], times = diff(   # repeat the values at these indices
-      c(ind, length(x) + 1) )) # diffing the indices + length yields how often 
-  } 
-  
-  #datas$MSG = repeat.before(datas$MSG)
-  datas[, MSG:=repeat.before(MSG)]
-  
-  datas = datas[Type == "SMP",]
+    
+    start = nchar(msgprefix)+1
+    datas[Type == "MSG", MSG:=substring(get(msgcol),start) ,by=Time]
+    #datas[Type == "MSG", MSG:=strsplit(get(msgcol),"# Message: ")[[1]][2] ,by=Time]
+    
+    repeat.before = function(x) {   # repeats the last non NA value. Keeps leading NA
+      ind = which(!is.na(x))      # get positions of nonmissing values
+      if(is.na(x[1]))             # if it begins with a missing, add the 
+        ind = c(1,ind)        # first position to the indices
+      rep(x[ind], times = diff(   # repeat the values at these indices
+        c(ind, length(x) + 1) )) # diffing the indices + length yields how often 
+    } 
+    
+    #datas$MSG = repeat.before(datas$MSG)
+    datas[, MSG:=repeat.before(MSG)]
+    
+    datas = datas[Type == "SMP",]
+    
+  }
   
   if (is.factor(datas[,msgcol,with=F][[1]])) {
     datas[, eval(msgcol):=as.character(get(msgcol))]
   }
-  
   datas <- datas[, eval(msgcol):=as.numeric(get(msgcol))] #make sure it is actually numeric!
   
   return(datas)
   
 }
 
-readFast = function(file) {
+readFast = function(file, msgcolposition=4) {
 
   result = tryCatch({
     
@@ -60,7 +66,7 @@ readFast = function(file) {
       classes[gottime][[1]] = "numeric"
     }
     
-    classes[4][[1]] = "character"
+    classes[msgcolposition][[1]] = "character"
     data.table(read.table(file = file, header = TRUE, sep = "\t", quote = "\"",  fill = TRUE, comment.char = "", skip=skip, colClasses = classes))
     
   }, warning = function(warn) {
