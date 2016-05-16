6003b948e03c69a87e1f3e854c16997b7f5ede84
thohag <tmshag1@gmail.com>
2016-05-16 23:36:56 +0200

    Experimental merging with event files. Updated manual.

--- R/readSMI.R
+++ R/readSMI.R
@@ -4,12 +4,10 @@
 #' @param file smi filename to read.
 #' @param msgcolposition column of MSG data (optional)
 #' @param msgprefix prefix to remove from MSG data (optional)
+#' @param eventsfile events file for merging with main file (optional / experimental)
 #' @examples
 #' data = readSMI("AXCPT_Subject_1 Samples.txt")
-
-
-
-readSMI = function(file, msgcolposition=4, msgprefix="# Message: ") {
+readSMI = function(file, msgcolposition=4, msgprefix="# Message: ", eventsfile=NULL) {
   
   datas <- readFast(file, msgcolposition)
   #datas <- data.table(read.csv(file=file, header=T, sep="\t", skip=38, stringsAsFactors=FALSE))
@@ -41,6 +39,10 @@ readSMI = function(file, msgcolposition=4, msgprefix="# Message: ") {
     datas[, eval(msgcol):=as.character(get(msgcol))]
   }
   datas <- datas[, eval(msgcol):=as.numeric(get(msgcol))] #make sure it is actually numeric!
+  
+  if (!is.null(eventsfile)) {
+    readEvents(eventsfile, datas)
+  }
   
   return(datas)
   
@@ -79,5 +81,45 @@ readFast = function(file, msgcolposition=4) {
   
   return(result)
   
+  
+}
+
+readEvents = function(eventsfile, data) {
+  
+  blinks <- read.csv(file=eventsfile, header=F, sep="\t", skip=20)
+  
+  repeat.before = function(x) {
+    ind = which(!is.na(x))
+    if(is.na(x[1])) ind = c(1,ind)
+    rep(x[ind], times = diff(c(ind, length(x) + 1) ))
+  }
+  
+  #Left
+  blinksL = blinks[blinks$V1 == "Blink L",]
+  if (nrow(blinksL) > 0) {
+    blinksL = transform(blinksL, V4 = as.numeric(as.character(V4)))
+    blinksL = transform(blinksL, V5 = as.numeric(as.character(V5)))
+    
+    data[Time %in% blinksL$V4,BlinkL:=1]
+    data[Time %in% blinksL$V5,BlinkL:=0]
+    
+    data[, BlinkL:=repeat.before(BlinkL)]
+    data[is.na(BlinkL),BlinkL:=0]
+  }
+
+  
+  
+  #Right
+  blinksR = blinks[blinks$V1 == "Blink R",]
+  if (nrow(blinksR) > 0) {
+    blinksR = transform(blinksR, V4 = as.numeric(as.character(V4)))
+    blinksR = transform(blinksR, V5 = as.numeric(as.character(V5)))
+    
+    data[Time %in% blinksR$V4,BlinkR:=1]
+    data[Time %in% blinksR$V5,BlinkR:=0]
+    
+    data[, BlinkR:=repeat.before(BlinkR)]
+    data[is.na(BlinkR),BlinkR:=0]
+  }
   
 }
